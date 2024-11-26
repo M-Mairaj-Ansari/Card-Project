a5e71eda52146085fa756fd1c3b149fa7b3836e4
M-Mairaj-Ansari <mairaja223@gmali.com>
2024-11-27 03:06:34 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
+"use client"
+import Image from "next/image"
 
-export default function Home() {
+import image from '../../public/product-Image/image.png'
+import image1 from '../../public/product-Image/image1.png'
+import image2 from '../../public/product-Image/image2.png'
+
+
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='flex justify-evenly pt-12'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+
+        <div className='border py-5 px-4 '>
+          <Image src={image} alt='a' width={200} height={150} />
+          <h1 className='text-center font-semibold text-xl mt-2'>Handsfree</h1>
+          <h2 className='text-center font-medium text-xl py-2 '>Rs: 2500</h2>
+          <div className='text-center'>
+            <button className=' bg-amber-600 py-2 px-8 rounded-lg text-lg'>Buy Now</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+        <div className='border py-5 px-4 '>
+          <Image src={image1} alt='a' width={200} height={150} />
+          <h1 className='text-center font-semibold text-xl'>Airpods pro </h1>
+          <h2 className='text-center font-medium text-xl py-2 '>Rs: 1500</h2>
+          <div className='text-center'>
+            <button className='bg-amber-600 py-2 px-8 rounded-lg text-lg' >Buy Now</button>
+          </div>
+        </div>
+
+        <div className='border py-5 px-4'>
+          <Image src={image2} alt='a' width={200} height={150} />
+          <h1 className='text-center font-semibold text-xl'>HeadPhone </h1>
+          <h2 className='text-center font-medium text-xl py-2 '>Rs: 4000</h2>
+          <div className='text-center'>
+            <button className='bg-amber-600 py-2 px-8 rounded-lg text-lg'>Buy Now</button>
+          </div>
+        </div>
+
+
+      </div>
     </div>
-  );
+
+
+
+  )
 }
+
+export default page
